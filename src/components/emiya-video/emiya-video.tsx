3730cbe760fb50f8afd21a92e0e87453dbd35d92
emiyalee <2288136aaa@gmail.com>
2024-10-25 04:37:35 +0800

    Update

--- src/components/emiya-video/emiya-video.tsx
+++ src/components/emiya-video/emiya-video.tsx
@@ -176,7 +176,7 @@ export class EmiyaVideo {
     );
     this.devToolsChangeListener = (a?: any) => {
       if (location.port !== '3333' && (devtools.isOpen || a?.detail?.isOpen)) {
-        location.href = 'about:blank';
+        //location.href = 'about:blank';
       }
     };
     window.addEventListener(
@@ -297,6 +297,9 @@ export class EmiyaVideo {
             onPointerCancel={() => this.onMouseLeave()}
           >
             <video
+              playsInline={true}
+              playsinline={true}
+              preload="auto"
               onContextMenu={a => {
                 a.preventDefault();
                 a.stopPropagation();
