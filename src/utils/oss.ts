df9921b05d657932c1c57db98547bd57478f3994
emiyalee <2288136aaa@gmail.com>
2024-11-05 02:36:13 +0800

    Update

--- src/utils/oss.ts
+++ src/utils/oss.ts
@@ -1,12 +1,17 @@
-import axios, { AxiosInstance } from 'axios';
-import { AxiosRequestConfig } from 'axios/index';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import pLimit from 'p-limit';
 
 export namespace OssUploader {
   export type ConstructionOptions = {
-    apiBaseUrl: string;
+    apiBaseUrl?: string;
+    chunkSize?: number;
+    concurrency?: number;
+    chunkFailureRetry?: number;
   };
   export type UploadOptions = {
-    apiBaseUrl: string;
+    file: File | Blob;
+    filename?: string;
+    onProgress: (event: { totalSize: number; uploadedSize: number; totalChunkCount: number; uploadedChunkCount: number; uploadedChunks: number[] }) => any;
   };
 }
 
@@ -20,12 +25,18 @@ type ApiResponseWrapper<T = any> = {
 };
 
 export class OssUploader {
+  private readonly chunkFailureRetry: number;
+  private readonly chunkSize: number;
   private axios: AxiosInstance;
   public mockEnabled: boolean = false;
   private readonly apiBaseUrl: string;
+  private readonly concurrency: number;
   constructor(options: OssUploader.ConstructionOptions) {
     this.apiBaseUrl = options.apiBaseUrl;
     this.axios = axios.create({ baseURL: this.apiBaseUrl });
+    this.chunkSize = options.chunkSize || 5 * 1024 * 1024;
+    this.concurrency = options.concurrency || 4;
+    this.chunkFailureRetry = options.chunkFailureRetry || 3;
   }
 
   private async request<T = Exclude<any, Function>>(config: AxiosRequestConfig, extraOptions?: { mockEnabled?: boolean; mockData?: T | (() => T) }) {
@@ -45,14 +56,101 @@ export class OssUploader {
   }
 
   private async createTask(options: { file: File | Blob; filename?: string }) {
-    return (
-      await this.request<{ videoId: string }>({
-        url: '/admin/createUploadTask',
-        method: 'post',
-        data: { name: options.filename || (options.file as File).name, fileSize: options.file.size },
-      })
-    ).videoId;
+    return await this.request<{ videoId: string; url: string }>({
+      url: '/admin/createUploadTask',
+      method: 'post',
+      data: { name: options.filename || (options.file as File).name, fileSize: options.file.size },
+    });
   }
 
-  public async upload(options: OssUploader.UploadOptions) {}
+  private async updateTask(options: { partNumber: number; eTag: string; videoId: string }) {
+    return await this.request({
+      url: '/admin/updateEtag',
+      method: 'post',
+      data: options,
+    });
+  }
+
+  private async completeTask(options: { videoId: string }) {
+    return await this.request({
+      url: '/admin/completeUploadTask',
+      method: 'post',
+      data: options,
+    });
+  }
+
+  public async getVideoDuration(options: { videoId: string }) {
+    return await this.request<number>({
+      url: '/admin/getDuration',
+      method: 'get',
+      params: options,
+    });
+  }
+
+  public async upload(options: OssUploader.UploadOptions) {
+    const chunkCount = Math.ceil(options.file.size / this.chunkSize);
+    const chunks: { start: number; end: number; size: number; partNum: number }[] = [];
+    for (let i = 0; i < chunkCount; i++) {
+      const start = i * this.chunkSize;
+      const end = Math.min(options.file.size, start + this.chunkSize);
+      chunks.push({
+        partNum: i + 1,
+        start,
+        end,
+        size: end - start,
+      });
+    }
+    const taskMeta = await this.createTask({ file: options.file, filename: options.filename });
+    const limit = pLimit(this.concurrency);
+
+    let uploadedChunkCount = 0,
+      uploadedSize = 0,
+      uploadedChunks: number[] = [];
+
+    const controller = new AbortController();
+
+    const chunkUploaders = chunks.map(chunk => {
+      return limit(async () => {
+        let retryLeft = this.chunkFailureRetry;
+        while (retryLeft) {
+          try {
+            if (controller.signal.aborted) throw new Error(`Aborted`);
+            const formData = new FormData();
+            const chunkFile = options.file.slice(chunk.start, chunk.end);
+            formData.append('file', chunkFile);
+            const res = await axios<{ eTag: string }>({ url: taskMeta.url, method: 'post', data: formData, params: { partNum: chunk.partNum } });
+            if (controller.signal.aborted) throw new Error(`Aborted`);
+            await this.updateTask({ videoId: taskMeta.videoId, partNumber: chunk.partNum, eTag: res.data.eTag });
+            if (controller.signal.aborted) throw new Error(`Aborted`);
+            ++uploadedChunkCount;
+            uploadedSize += chunkFile.size;
+            uploadedChunks.push(chunk.partNum);
+            try {
+              options.onProgress &&
+                options.onProgress({
+                  uploadedSize,
+                  uploadedChunkCount,
+                  uploadedChunks,
+                  totalChunkCount: chunks.length,
+                  totalSize: options.file.size,
+                });
+            } catch (e) {
+              console.error(e);
+            }
+            return res.data.eTag;
+          } catch (e) {
+            --retryLeft;
+            if (!retryLeft || controller.signal.aborted) {
+              controller.abort(`Chunk ${chunk.partNum} upload failed after ${this.chunkFailureRetry} attempts with error : ${e.message}`);
+              throw e;
+            }
+          }
+        }
+      });
+    });
+
+    await Promise.all(chunkUploaders);
+    await this.completeTask({ videoId: taskMeta.videoId });
+    return taskMeta.videoId;
+  }
 }
